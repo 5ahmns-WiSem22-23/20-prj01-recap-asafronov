6b51c08ff346f74c38d940f795989d1598a75bec
Pacifist-Player28 <andrey.schule@gmail.com>
2022-12-26 19:57:03 +0100

    Adding minimap function

---
+++ Assets/MyGame/Scripts/MinimapScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        transform.position = player.position + new Vector3(0f, 0f, -10f);
+        //transform.position = newPosition;
+
+        //Debug.Log(transform.position.y);
+    }
+}
